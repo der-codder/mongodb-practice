06a1f6323844439f8f6b86ca5f8e0a82755e891b
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2017-12-21 09:13:19 +0200

    Removing Subdocuments

--- users/test/subdocument_test.js
+++ users/test/subdocument_test.js
@@ -34,4 +34,23 @@ describe('Subdocuments', () => {
         done();
       })
   });
+
+  it ('can remove subdocuments to an existing record', (done) => {
+    const joe = new User({
+      name: 'Joe',
+      posts: [{ title: 'New Post' }]
+    });
+    
+    joe.save()
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        user.posts[0].remove();
+        return user.save();
+      })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.posts.length === 0);
+        done();
+      })
+  });
 });
