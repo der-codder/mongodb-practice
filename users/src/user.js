7c9254aec6422bc7705a3b13e2e6f380c37c66ae
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2017-12-25 10:06:59 +0200

    Pre-Remove Middleware

--- users/src/user.js
+++ users/src/user.js
@@ -24,6 +24,13 @@ UserSchema.virtual('postCount')
     return this.posts.length;
   });
 
+UserSchema.pre('remove', function(next) {
+  const BlogPost = mongoose.model('blogPost');
+
+  BlogPost.remove({ _id: { $in: this.blogPosts } })
+    .then(() => next());
+});
+
 const User = mongoose.model('user', UserSchema);
 
 module.exports = User;
