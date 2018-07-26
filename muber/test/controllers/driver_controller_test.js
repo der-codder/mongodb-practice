8df8456a6ec92d916c59246202f661389b33d24d
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2018-07-26 10:56:11 +0300

    119. Testing Driver Updates

--- muber/test/controllers/driver_controller_test.js
+++ muber/test/controllers/driver_controller_test.js
@@ -9,13 +9,35 @@ describe('Driver controller', () => {
   it('Post to /api/drivers creates a new driver.', done => {
     Driver.count()
       .then(count => {
-        request(app).post('/api/drivers')
+        request(app)
+          .post('/api/drivers')
           .send({ email: 'test@test.com' })
           .end(() => {
             Driver.count()
               .then(newCount => {
                 assert(count + 1 === newCount);
                 done();
+              });
+          });
+      });
+  });
+
+  it('PUT to /api/drivers/id edits an existing driver', done => {
+    const driver = new Driver({ email: 't@t.com', driving: false });
+
+    driver.save()
+      .then(() => {
+        request(app)
+          .put(`/api/drivers/${driver._id}`)
+          .send({ driving: true })
+          .end(() => {
+            Driver.findOne({ email: 't@t.com' })
+              .then(driver => {
+                assert(driver.driving === true);
+                done();
+              })
+              .catch(err => {
+                console.log(err);
               });
           });
       });
